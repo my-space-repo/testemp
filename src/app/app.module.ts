aa62f8f2b79df09ec77fd5ed21c3622f86d3e95c
subr1990 <subramaniank@live.com>
2020-09-05 22:35:48 +1000

    tets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClientService} from './service/http-client.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ HttpClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
